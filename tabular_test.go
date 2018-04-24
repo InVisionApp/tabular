13af470c847e7757420ba2e3e3e29e175af9261f
vmogilev <oracle.ux@gmail.com>
2018-04-23 17:45:43 -0700

    handle RightJustified test

--- tabular_test.go
+++ tabular_test.go
@@ -11,14 +11,24 @@ func TestTabular(t *testing.T) {
 		"cls": &Column{Name: "Cluster", Length: 10},
 		"svc": &Column{Name: "Service", Length: 25},
 		"hst": &Column{Name: "Database Host", Length: 25},
+		"pct": &Column{Name: "PCT", Length: 5, RightJustified: true},
 	}
 
 	got := New()
 	for k, v := range want {
 		got.Add(k, v.Name, v.Length)
+		got[k].RightJustified = v.RightJustified
 	}
 
 	if !reflect.DeepEqual(want, got) {
-		t.Errorf("ERROR: tabular.Add() failed\n  want: %#v\n   got: %#v", got, want)
+		if len(got) != len(want) {
+			t.Fatalf("ERROR: tabular.Add() failed to produce map of the right size")
+		}
+		for k, v := range want {
+			if !reflect.DeepEqual(v, got[k]) {
+				t.Errorf("ERROR: tabular.Column mismatch\n  want: %#v\n   got: %#v", *v, *got[k])
+			}
+		}
+
 	}
 }
